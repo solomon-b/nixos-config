574de232e8a011e41d2aa141a1a0a4b2b2cfa9a8
solomon <ssbothwell@gmail.com>
2021-11-17 19:35:04 -0800

    pins xmobar to 0.40

--- flakes/xmobar-solomon/overlay.nix
+++ flakes/xmobar-solomon/overlay.nix
@@ -3,19 +3,16 @@ final: prev: {
     overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
     (hfinal: hprev: {
 
-     xmobar-solomon = hfinal.callCabal2nix "xmobar-solomon" (./.) { };
-      #xmobar-solomon = hfinal.callCabal2nix "xmobar-solomon" (
-      #  final.lib.sourceByRegex ./.
-      #  [
-      #    "./app/Main.hs"
-      #    "./src/App/Acpi.hs"
-      #    "./src/App/Icons.hs"
-      #    "./src/App/DunstStatus.hs"
-      #    "cabal.project"
-      #    "xmobar-solomon.cabal"
-      #    "LICENSE"
-      #  ]
-      #) { };
+      xmobar =
+        let xmobar = prev.fetchFromGitHub {
+              owner = "jaor";
+              repo = "xmobar";
+              rev = "397953f1c626a3a81b9ef7280d961fb3ce340c56";
+              sha256 = "sha256-O42xW58EJBE7BIrApHF7EXlWoOeosLcn/cCy42aABrA=";
+            };
+        in final.haskell.lib.dontCheck (hfinal.callCabal2nix "xmobar" xmobar { });
+
+      xmobar-solomon = hfinal.callCabal2nix "xmobar-solomon" (./.) { };
     });
   });
 }
