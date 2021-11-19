4263a10a161a20028ef98b8919c59caf98f3ddde
ssbothwell <ssbothwell@gmail.com>
2021-11-19 12:45:42 -0800

    Updates postgres to 13

--- config/modules/system/postgresql/default.nix
+++ config/modules/system/postgresql/default.nix
@@ -3,7 +3,8 @@
 {
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_12;
+    package = pkgs.postgresql_13;
+    extraPlugins = [ pkgs.postgresql_13.pkgs.postgis ];
     settings = {
       shared_preload_libraries = "pg_stat_statements";
     };
