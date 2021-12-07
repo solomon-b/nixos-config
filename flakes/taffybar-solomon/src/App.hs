d8ee805ae635d67ef91fe653dacc47fe1b9db9a8
solomon <ssbothwell@gmail.com>
2021-12-07 00:13:21 -0800

    Updates flake locks and adds taffy bar wip

---
+++ flakes/taffybar-solomon/src/App.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+module App where
+
+import System.Taffybar
+import System.Taffybar.Information.CPU
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Generic.Graph
+import System.Taffybar.Widget.Generic.PollingGraph
+
+cpuCallback :: IO [Double]
+cpuCallback = do
+  (_, systemLoad, totalLoad) <- cpuLoad
+  return [ totalLoad, systemLoad ]
+
+runTaffy :: IO ()
+runTaffy =
+  let cpuCfg = defaultGraphConfig { graphDataColors = [ (0, 1, 0, 1), (1, 0, 1, 0.5)]
+                                  , graphLabel = Just "cpu"
+                                  }
+      clock = textClockNewWith defaultClockConfig
+      cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      workspaces = workspacesNew defaultWorkspacesConfig
+      simpleConfig = defaultSimpleTaffyConfig
+                       { startWidgets = [ workspaces ]
+                       , endWidgets = [ sniTrayNew, clock, cpu ]
+                       }
+  in simpleTaffybar simpleConfig
