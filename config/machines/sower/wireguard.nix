2d1171b23330be0df0d806b8cbef32f31ace4189
solomon <ssbothwell@gmail.com>
2022-04-27 11:26:25 -0700

    Removes unneeded rules from sower iptables.

--- config/machines/sower/wireguard.nix
+++ config/machines/sower/wireguard.nix
@@ -8,16 +8,6 @@
 
     # if packets are still dropped, they will show up in dmesg
     logReversePathDrops = true;
-
-    # wireguard trips rpfilter up
-    extraCommands = ''
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-      ip46tables -t raw -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-      ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-    '';
   };
 
   networking.wg-quick.interfaces = {
