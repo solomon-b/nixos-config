425dc32f1478f12f5f682fd48f84985f2cdb15ee
solomon <ssbothwell@gmail.com>
2022-06-07 11:12:11 -0700

    Modify nginx reverse proxy for magicdns support

--- config/machines/sower/nginx.nix
+++ config/machines/sower/nginx.nix
@@ -5,26 +5,19 @@
       enable = true;
       recommendedProxySettings = true;
       virtualHosts = {
-        "qbittorrent.sower" =  {
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:8080";
-          };
-        };
-
-        "syncthing.sower" = {
-          locations."/" = {
-            proxyPass = "http://localhost:8384";
-          };
-        };
-
-        "jellyfin.sower" =  {
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:8096";
-          };
-        };
-
         "sower" = {
           root = "/srv/www/sower.galaxybrain.zone";
+          locations = {
+            "/jellyfin" = {
+              proxyPass = "http://100.80.98.4:8096";
+            };
+            "/qbittorrent" = {
+              proxyPass = "http://100.80.98.4:8080";
+            };
+            "/syncthing" = {
+              proxyPass = "http://100.80.98.4:8384";
+            };
+          };
         };
       };
   };
