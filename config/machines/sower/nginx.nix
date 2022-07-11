c2dafe415f3580749b59bcfc3c11f3a89646ca84
solomon <ssbothwell@gmail.com>
2022-07-10 22:07:15 -0700

    remove virtual hosts from sower/nginx.nix

--- config/machines/sower/nginx.nix
+++ config/machines/sower/nginx.nix
@@ -7,14 +7,6 @@
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
-
-    virtualHosts = {
-      "sower" = {
-        root = "/srv/www/sower.galaxybrain.zone";
-      };
-      "nextcloud.sower" = {
-      };
-    };
   };
 
   networking.firewall.allowedTCPPorts = [ 80 ];
