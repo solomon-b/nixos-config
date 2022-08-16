02c4a1e870f9347090bec420be4ef1c16b12b939
solomon <ssbothwell@gmail.com>
2022-08-15 22:52:56 -0700

    Adds nix-direnv

--- config/profiles/pc/default.nix
+++ config/profiles/pc/default.nix
@@ -32,6 +32,7 @@
   environment.systemPackages = with pkgs; [
     # CLI Tools
     ispell
+    nix-direnv
     udiskie
     sqlite # for org-roam, should I move to postgres? How would I sync between machines in that case?
     xclip
