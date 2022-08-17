b0fee09455af0bab8b8008348ca4428f589324b8
solomon <ssbothwell@gmail.com>
2022-08-17 13:03:00 -0700

    Update NFS shares

--- config/machines/nightshade/nfs.nix
+++ config/machines/nightshade/nfs.nix
@@ -2,7 +2,12 @@
 
 {
   fileSystems."/mnt/media" = {
-    device = "192.168.1.174:/mnt/tank/Media ";
+    device = "192.168.1.174:/mnt/tank/Media";
+    fsType = "nfs";
+  };
+
+  fileSystems."/mnt/storage" = {
+    device = "192.168.1.174:/mnt/tank/solomon";
     fsType = "nfs";
   };
 }
--- config/machines/sower/nfs.nix
+++ config/machines/sower/nfs.nix
@@ -5,4 +5,9 @@
     device = "192.168.1.174:/mnt/tank/Media ";
     fsType = "nfs";
   };
+
+  fileSystems."/mnt/storage" = {
+    device = "192.168.1.174:/mnt/tank/solomon";
+    fsType = "nfs";
+  };
 }
