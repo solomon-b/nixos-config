10d701ceaf752248a39953d2cf5d7bd300230d41
solomon <ssbothwell@gmail.com>
2022-08-22 23:24:07 -0700

    Remove old overlay

--- flakes/xmobar-solomon/overlay.nix
+++ flakes/xmobar-solomon/overlay.nix
@@ -2,16 +2,6 @@ final: prev: {
   haskellPackages = prev.haskellPackages.override (old: {
     overrides = prev.lib.composeExtensions (old.overrides or (_: _: {}))
     (hfinal: hprev: {
-
-      xmobar =
-        let xmobar = prev.fetchFromGitHub {
-              owner = "jaor";
-              repo = "xmobar";
-              rev = "dbe9cba77dd23910103aeb667ef07377ceec9464";
-              sha256 = "sha256-uWke/S76FiANCiOg+2RhVoGRyH7It32AAEWuUCUpUpM=";
-            };
-        in final.haskell.lib.dontCheck (hfinal.callCabal2nix "xmobar" xmobar { });
-
       xmobar-solomon = hfinal.callCabal2nix "xmobar-solomon" (./.) { };
     });
   });
