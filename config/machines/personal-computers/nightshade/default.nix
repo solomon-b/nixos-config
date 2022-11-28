8f19526e2a8a82ddec3d5b272de6db17efda8069
solomon <ssbothwell@gmail.com>
2022-11-27 19:17:23 -0800

    Enable printing on nightshade

--- config/machines/personal-computers/nightshade/default.nix
+++ config/machines/personal-computers/nightshade/default.nix
@@ -38,4 +38,10 @@
     useDHCP = false;
     interfaces.wlp170s0.useDHCP = true;
   };
+
+  services.printing.enable = true;
+  services.avahi.enable = true;
+  # Important to resolve .local domains of printers, otherwise you get an error
+  # like  "Impossible to connect to XXX.local: Name or service not known"
+  services.avahi.nssmdns = true;
 }
