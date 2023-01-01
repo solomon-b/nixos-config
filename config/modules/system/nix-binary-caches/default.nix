1c0de25d35c2b79b6795c5afb1630ed804aa3c55
solomon <ssbothwell@gmail.com>
2022-12-31 17:14:18 -0800

    Disable hydra.iohk.io binary cache

--- config/modules/system/nix-binary-caches/default.nix
+++ config/modules/system/nix-binary-caches/default.nix
@@ -4,12 +4,12 @@
   nix = {
     binaryCaches = [
       "https://nix-linter.cachix.org"
-      "https://hydra.iohk.io"
+      #"https://hydra.iohk.io"
       "https://iohk.cachix.org"
     ];
     binaryCachePublicKeys = [
       "nix-linter.cachix.org-1:BdTne5LEHQfIoJh4RsoVdgvqfObpyHO5L0SCjXFShlE="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      #"hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     ];
   };
