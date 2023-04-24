2fd7aa49270df77e8b575d4f4c2cc56009c24ad9
solomon <ssbothwell@gmail.com>
2023-04-23 21:54:30 -0700

    switch device id from sda to vda in installer script

--- installer/install.sh
+++ installer/install.sh
@@ -3,16 +3,16 @@
 
 set -ex
 
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart primary 512MiB -8GiB
-parted /dev/sda -- mkpart primary linux-swap -8GiB 100%
-parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
-parted /dev/sda -- set 3 boot on
+parted /dev/vda -- mklabel gpt
+parted /dev/vda -- mkpart primary 512MiB -8GiB
+parted /dev/vda -- mkpart primary linux-swap -8GiB 100%
+parted /dev/vda -- mkpart ESP fat32 1MiB 512MiB
+parted /dev/vda -- set 3 boot on
 
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-swapon /dev/sda2
-mkfs.fat -F 32 -n boot /dev/sda3        # (for UEFI systems only)
+mkfs.ext4 -L nixos /dev/vda1
+mkswap -L swap /dev/vda2
+swapon /dev/vda2
+mkfs.fat -F 32 -n boot /dev/vda3        # (for UEFI systems only)
 mount /dev/disk/by-label/nixos /mnt
 mkdir -p /mnt/boot                      # (for UEFI systems only)
 mount /dev/disk/by-label/boot /mnt/boot # (for UEFI systems only)
