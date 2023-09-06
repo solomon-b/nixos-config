ca84e756bc0fc56be20616c9bdf6d278cfdda459
solomon <ssbothwell@gmail.com>
2023-09-06 01:04:41 -0700

    Adds anbox to nightshade

--- config/machines/personal-computers/nightshade/default.nix
+++ config/machines/personal-computers/nightshade/default.nix
@@ -9,6 +9,7 @@
     ./zfs.nix
 
     ../../../profiles/pc
+    ../../../modules/services/virtualisation/anbox
     ../../../modules/services/virtualisation/libvirt
     ../../../modules/system/devices/touchpad
     ../../../modules/system/powertop
