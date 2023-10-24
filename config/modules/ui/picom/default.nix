786adb4dfdf666fc616d2b4edebd2a1a52aba676
solomon <ssbothwell@gmail.com>
2023-10-23 22:51:28 -0700

    Experimenting with picom config for eww.

--- config/modules/ui/picom/default.nix
+++ config/modules/ui/picom/default.nix
@@ -7,7 +7,18 @@
     inactiveOpacity = 0.9;
     fade = false;
     fadeDelta = 5;
+    settings = {
+      corner-radius = 10;
+      rounded-corners-exclude = "class_g != 'eww-acpi_info'";
+    };
     shadow = true;
     shadowOpacity = 0.75;
+    # https://www.reddit.com/r/suckless/comments/g689o8/removing_comptonpicom_shadow_from_status_bar/
+    shadowExclude = [
+      "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'"
+      "class_g = 'eww-ewwbar'"
+      "class_g = 'trayer'"
+      "class_g = 'xmonad-decoration'"
+    ];
   };
 }
