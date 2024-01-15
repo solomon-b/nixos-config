1168ad6482412807b9a93671a3fbb6dca88e90c8
solomon <ssbothwell@gmail.com>
2024-01-14 20:37:49 -0800

    Add session variables for XDG paths.

--- config/modules/data/session-vars/default.nix
+++ config/modules/data/session-vars/default.nix
@@ -4,17 +4,23 @@ let
   homeDir = config.primary-user.home-manager.home.homeDirectory;
 in
 {
-  primary-user.home-manager.home = {
+  primary-user.home-manager.home = rec {
     sessionPath = [
-      "${homeDir}/.local/scripts"
-      "${homeDir}/.local/bin"
       "${homeDir}/.cabal/bin"
+      "${sessionVariables.XDG_BIN_HOME}"
     ];
 
     sessionVariables = {
       VISUAL = "vim";
       EDITOR = "vim";
-      SCRIPTS = "${homeDir}/.local/scripts";
+
+      XDG_CACHE_HOME  = "${homeDir}/.cache";
+      XDG_CONFIG_HOME = "${homeDir}/.config";
+      XDG_DATA_HOME   = "${homeDir}/.local/share";
+      XDG_STATE_HOME  = "${homeDir}/.local/state";
+      
+      # Not officially in the specification
+      XDG_BIN_HOME    = "${homeDir}/.local/bin";
     };
   };
 }
