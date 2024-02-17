2ff7ccf79ad4d21910078812bee43e1dfc087d8e
solomon <ssbothwell@gmail.com>
2024-02-16 21:25:02 -0800

    Updates git config

--- config/modules/ui/git/default.nix
+++ config/modules/ui/git/default.nix
@@ -6,8 +6,39 @@
     userName = "solomon";
     userEmail = "ssbothwell@gmail.com";
     extraConfig = {
+      branch = {
+        sort = "-committerdate";
+      };
+      column = {
+        ui = "auto";
+      };
+      gpg = {
+        format = "ssh";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      merge = {
+        # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#merge-conflictstyle-zdiff3
+        conflictstyle = "zdiff3";
+      };
+      pull = {
+        # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#pull-ff-only-or-pull-rebase-true
+        rebase = true;
+      };
+      push = {
+        # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#push-default-simple-push-default-current
+        autoSetupRemote = true;
+      };
+      # https://jvns.ca/blog/2024/02/16/popular-git-config-options/#rerere-enabled-true
+      rerere = {
+        enabled = true;
+      };
       safe = {
         directory = "/etc/nixos/flake";
+      };
+      user = {
+        signingKey = "~/.ssh/id_ed25519.pub";
       };
     };
     includes = [
