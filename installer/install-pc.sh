7e954a61b6cf1146eec47c0fc339270c0d05822c
solomon <ssbothwell@gmail.com>
2024-08-16 11:10:59 -0700

    Updates install-pc script.

--- installer/install-pc.sh
+++ installer/install-pc.sh
@@ -17,33 +17,37 @@ main () {
     MACHINE=$(ls config/machines/personal-computers | gum choose)
     IP=$(gum input --placeholder "enter address..")
 
-    # Create a temporary directory
+    gum log --level info "Create a temporary directory"
     temp=$(mktemp -d)
     trap cleanup EXIT
 
-    # Create the directory where sshd expects to find the host keys
+    gum log --level info "Create the directory where sshd expects to find the host keys"
     install -d -m755 "$temp/etc/ssh"
 
-    # Decrypt your SSH host keys from the password store and copy it to the temporary directory
+    gum log --level info "Decrypt your SSH host keys from the password store and copy it to the temporary directory"
     pass "machine/${MACHINE}/ssh-host-key/ed25519/private" > "$temp/etc/ssh/ssh_host_ed25519_key"
     pass "machine/${MACHINE}/ssh-host-key/ed25519/public" > "$temp/etc/ssh/ssh_host_ed25519_key.pub"
     pass "machine/${MACHINE}/ssh-host-key/rsa/private" > "$temp/etc/ssh/ssh_host_rsa_key"
     pass "machine/${MACHINE}/ssh-host-key/rsa/public" > "$temp/etc/ssh/ssh_host_rsa_key.pub"
 
-    # Set the correct permissions so sshd will accept the keys
+    gum log --level info "Set the correct permissions so sshd will accept the keys"
     chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
     chmod 644 "$temp/etc/ssh/ssh_host_ed25519_key.pub"
     chmod 600 "$temp/etc/ssh/ssh_host_rsa_key"
     chmod 644 "$temp/etc/ssh/ssh_host_rsa_key.pub"
 
-    # Install NixOS to the host system with our secrets
+    gum log --level info "Install NixOS to the host system with our secrets"
     nix run github:numtide/nixos-anywhere -- --extra-files "$temp" --flake ".#${MACHINE}" "root@${IP}" --no-reboot
 
-    # Copy User SSH keys
+    gum log --level info "Mount ZFS datasets to /mnt"
     ssh "root@${IP}" <<EOF
+      mount.zfs tank/root /mnt
+      mount.zfs tank/systemd-logs /var/log
+      mount.zfs tank/nix /mnt/nix
       mount.zfs tank/home /mnt/home
       mkdir -p /mnt/home/solomon/.ssh
 EOF
+    gum log --level info "Copy User SSH keys"
     pass "machine/${MACHINE}/solomon/ssh/private-key" | ssh "root@${IP}" 'cat > /mnt/home/solomon/.ssh/id_ed25519'
     pass "machine/${MACHINE}/solomon/ssh/public-key" | ssh "root@${IP}" 'cat > /mnt/home/solomon/.ssh/id_ed25519.pub'
     ssh "root@${IP}" <<EOF
@@ -51,11 +55,11 @@ EOF
       chown -R 1000:100 /mnt/home/solomon/.ssh
 EOF
 
-    # Copy flake repo
+    gum log --level info "Copy flake repo"
     rsync -chavzP  /etc/nixos/flake "root@${IP}:/mnt/etc/nixos/"
     ssh "root@${IP}" 'chown -R 1000:100 /mnt/etc/nixos/flake'
 
-    # Copy GPG
+    gum log --level info "Copy GPG"
     rsync -chavzP /home/solomon/.gnupg "root@${IP}:/mnt/home/solomon/"
 
     ssh "root@${IP}" 'reboot now'
