7fa3a44fd28b05700dcade2d5e5fc8fa329a1893
solomon <ssbothwell@gmail.com>
2024-10-15 11:57:25 -0700

    Adds home-manager udiskie module

--- config/modules/system/devices/udisk/default.nix
+++ config/modules/system/devices/udisk/default.nix
@@ -5,4 +5,20 @@
     enable = true;
     mountOnMedia = true;
   };
+
+  services.dbus.enable = true;
+
+  primary-user.home-manager = {
+    services.udiskie = {
+      enable = true;
+    };
+
+    # https://github.com/nix-community/home-manager/issues/2064
+	  systemd.user.targets.tray = {
+	  	Unit = {
+	  		Description = "Home Manager System Tray";
+	  		Requires = [ "graphical-session-pre.target" ];
+	  	};
+    };
+	};
 }
