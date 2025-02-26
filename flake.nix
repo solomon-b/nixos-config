b7a21d175faa9669b647368d2add584344784370
solomon <ssbothwell@gmail.com>
2025-02-26 08:27:36 -0800

    Set nixpkgs.config once.

--- flake.nix
+++ flake.nix
@@ -83,6 +83,12 @@
 
         config = {
           allowUnfree = true;
+          permittedInsecurePackages = [
+            "aspnetcore-runtime-6.0.36"
+            "aspnetcore-runtime-wrapped-6.0.36"
+            "dotnet-sdk-6.0.428"
+            "dotnet-sdk-wrapped-6.0.428"
+          ];
         };
 
         overlays = with inputs; [
