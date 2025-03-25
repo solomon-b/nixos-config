99d6267b81b1d4702684957a943c505433d24201
solomon <ssbothwell@gmail.com>
2025-03-24 23:34:31 -0700

    Adds postprocess-ipod lidarr script.

--- config/machines/servers/accompaniment-of-shadows/servarr.nix
+++ config/machines/servers/accompaniment-of-shadows/servarr.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  imports = [
+    ./ipod-processing.nix
+  ];
+
   services = {
     bazarr = {
       enable = true;
@@ -31,17 +35,17 @@
 
   environment.systemPackages = [
       pkgs.ffmpeg
-      (let src = builtins.readFile ./postprocess-ipod.sh;
-           script = (pkgs.writeScriptBin "postproces-ipod" src).overrideAttrs (old: {
-              buildCommand = "${old.buildCommand}\n patchShebangs $out";
-           });
-        in
-        pkgs.symlinkJoin {
-          name = "postproces-ipod";
-          paths = [ pkgs.ffmpeg pkgs.iconv script  ];
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/postprocess-ipod --prefix PATH : $out/bin";
-        })
+      # (let src = builtins.readFile ./postprocess-ipod.sh;
+      #      script = (pkgs.writeScriptBin "postproces-ipod" src).overrideAttrs (old: {
+      #         buildCommand = "${old.buildCommand}\n patchShebangs $out";
+      #      });
+      #   in
+      #   pkgs.symlinkJoin {
+      #     name = "postprocess-ipod";
+      #     paths = [ pkgs.ffmpeg pkgs.iconv script  ];
+      #     buildInputs = [ pkgs.makeWrapper ];
+      #     postBuild = "wrapProgram $out/bin/postprocess-ipod --prefix PATH : $out/bin";
+      #   })
     ];
 
   # https://github.com/NixOS/nixpkgs/issues/155475#issuecomment-1093940244
