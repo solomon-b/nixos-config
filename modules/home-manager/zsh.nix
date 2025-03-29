e6b70d965950edac9f667c4961b96cff0c82fff1
solomon <ssbothwell@gmail.com>
2025-03-29 10:57:34 -0700

    all: modify git-jump and add fzf/zsh integration.

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -32,25 +32,21 @@
     ];
 
     initExtra = ''
-      bindkey '^l' autosuggest-accept
-      bindkey '^h' autosuggest-clear
-      bindkey '^k' history-substring-search-up
-      bindkey '^j' history-substring-search-down
+       function git-jump() {
+         local REPO
+         REPO=$(fd -H -t d '.git' ${config.home.homeDirectory}/Development -d 5 -E '.github' | sed 's|/\.git/$||' | awk '{ print; system("dirname \"" $0 "\"") }' | sed 's|^${config.home.homeDirectory}/Development/||' | sort -u | fzf --height 25)
 
-      autoload edit-command-line
-      zle -N edit-command-line 
-      bindkey “^x^e” edit-command-line
+         if [[ -n $REPO ]]; then
+           local DEST="${config.home.homeDirectory}/Development/''${REPO}"
 
-      # Load Functions
-      if [ -f ${config.xdg.configHome}/functions ]; then
-          source ${config.xdg.configHome}/functions
-      fi
-[ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+         cd "$DEST" || return
+         # Refresh prompt after changing directory
+         zle reset-prompt
 
-      function git-jump {
-        REPO=$(fd -t d -d 3 . ${config.home.homeDirectory}/Development | sed -E 's|^${config.home.homeDirectory}/Development/||g' | fzf)
-        [[ -n $REPO ]] && cd "${config.home.homeDirectory}/Development/''${REPO}"
-      }
+         fi
+       }
+
+      zle -N git-jump
 
       function jump-to-git-root {
         local ROOT_DIR="$(git rev-parse --show-toplevel 2>/dev/null)"
@@ -58,20 +54,38 @@
       }
 
       function fzf-checkout {
-          BRANCH=$(git branch | fzf| tr -d '[:space:]')
+          BRANCH=$(git branch | fzf --height 25| tr -d '[:space:]')
           [[ -n $BRANCH ]] && git checkout $BRANCH
       }
-      
+
       function git-checkout-branch {
           $(git rev-parse --is-inside-work-tree >& /dev/null)
-      
+
           if [ "$?" -eq "0" ]; then
               [[ -n $1 ]] && git checkout $1 || fzf-checkout
           fi
       }
+
+      bindkey '^l' autosuggest-accept
+      bindkey '^h' autosuggest-clear
+      bindkey '^k' history-substring-search-up
+      bindkey '^j' history-substring-search-down
+      #bindkey '^R' history-incremental-search-backward
+      bindkey '^g' git-jump
+
+      autoload edit-command-line
+      zle -N edit-command-line
+      bindkey “^x^e” edit-command-line
+
+      # Load Functions
+      if [ -f ${config.xdg.configHome}/functions ]; then
+          source ${config.xdg.configHome}/functions
+      fi
+[ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
     '';
 
     shellAliases = {
+      cursor = "$HOME/.local/bin/cursor";
       refresh = "exec $SHELL -l";
       g = "git-jump";
       gr = "jump-to-git-root";
@@ -91,5 +105,8 @@
       "rmd" = "rm -r";
     };
   };
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 }
-
