a81c505942f0060778f883a41a520ef25f06d4b5
solomon <ssbothwell@gmail.com>
2025-05-17 23:08:26 -0700

    sower: update nfs mount options.

--- config/machines/servers/sower/tubearchivist.nix
+++ config/machines/servers/sower/tubearchivist.nix
@@ -8,11 +8,35 @@ in
   fileSystems."/mnt/tubearchivist/media" = {
     device = "192.168.5.6:/mnt/tank/app-data/tube-archivist/media";
     fsType = "nfs";
+    options = [
+      "defaults"                  # → rw,suid,dev,exec,auto,nouser,async
+      "vers=3"                    # force NFSv3
+      "proto=tcp"                 # use TCP transport
+      "soft"                      # give up after retries (don’t hang forever)
+      "intr"                      # allow signals (Ctrl-C) to interrupt
+      "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
+      "retrans=3"                 # retry only 3 times (~9 s total)
+      "_netdev"                   # wait for network before mounting
+      #"x-systemd.automount"       # lazy-mount on first access
+      #"x-systemd.idle-timeout=60" # auto-unmount after 60 s idle
+    ];
   };
 
   fileSystems."/mnt/tubearchivist/cache" = {
     device = "192.168.5.6:/mnt/tank/app-data/tube-archivist/cache";
     fsType = "nfs";
+    options = [
+      "defaults"                  # → rw,suid,dev,exec,auto,nouser,async
+      "vers=3"                    # force NFSv3
+      "proto=tcp"                 # use TCP transport
+      "soft"                      # give up after retries (don’t hang forever)
+      "intr"                      # allow signals (Ctrl-C) to interrupt
+      "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
+      "retrans=3"                 # retry only 3 times (~9 s total)
+      "_netdev"                   # wait for network before mounting
+      #"x-systemd.automount"       # lazy-mount on first access
+      #"x-systemd.idle-timeout=60" # auto-unmount after 60 s idle
+    ];
   };
 
   virtualisation.oci-containers.containers = {
