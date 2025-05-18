f2705551d7e6c2883315c992ba101836c832ea82
solomon <ssbothwell@gmail.com>
2025-05-18 11:46:35 -0700

    Updates paperless dns and nfs.

--- config/machines/servers/accompaniment-of-shadows/paperless-ngx.nix
+++ config/machines/servers/accompaniment-of-shadows/paperless-ngx.nix
@@ -4,6 +4,18 @@
   fileSystems."/mnt/paperless-ngx" = {
     device = "192.168.5.6:/mnt/tank/app-data/paperless-ngx";
     fsType = "nfs";
+    options = [
+      "defaults"                  # → rw,suid,dev,exec,auto,nouser,async
+      "vers=3"                    # force NFSv3
+      "proto=tcp"                 # use TCP transport
+      "soft"                      # give up after retries (don’t hang forever)
+      "intr"                      # allow signals (Ctrl-C) to interrupt
+      "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
+      "retrans=3"                 # retry only 3 times (~9 s total)
+      "_netdev"                   # wait for network before mounting
+      # "x-systemd.automount"       # lazy-mount on first access
+      # "x-systemd.idle-timeout=60" # auto-unmount after 60 s idle
+    ];
   };
 
   services.paperless = {
@@ -23,7 +35,7 @@
     paperless-password = {};
   };
 
-  services.nginx.virtualHosts."paperless.service" = {
+  services.nginx.virtualHosts."paperless.service.home.arpa" = {
     locations."/".proxyPass = "http://localhost:9000";
   };
 }
