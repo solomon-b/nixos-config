588ad675ba8ddfcc7f99fd5bf72af11c21b94766
solomon <ssbothwell@gmail.com>
2025-05-21 01:32:26 -0700

    servers: fixes remaining NFS mount options.

--- config/machines/servers/accompaniment-of-shadows/homebox.nix
+++ config/machines/servers/accompaniment-of-shadows/homebox.nix
@@ -7,6 +7,15 @@ in
   fileSystems."/mnt/homebox" = {
     device = "192.168.5.6:/mnt/tank/app-data/homebox";
     fsType = "nfs";
+    options = [
+      "defaults"                  # → rw,suid,dev,exec,auto,nouser,async
+      "vers=3"                    # force NFSv3
+      "proto=tcp"                 # use TCP transport
+      "intr"                      # allow signals (Ctrl-C) to interrupt
+      "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
+      "retrans=3"                 # retry only 3 times (~9 s total)
+      "_netdev"                   # wait for network before mounting
+    ];
   };
 
   virtualisation.oci-containers.containers = {
