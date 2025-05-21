e0cc578053d0ef91c11b5e01ec2394730514c04d
solomon <ssbothwell@gmail.com>
2025-05-21 01:32:26 -0700

    Fixes remaining NFS mount options.

--- config/machines/servers/apollyon/default.nix
+++ config/machines/servers/apollyon/default.nix
@@ -20,5 +20,14 @@
   fileSystems."/mnt/media" = {
     device = "192.168.5.6:/mnt/tank/Media";
     fsType = "nfs";
+    options = [
+      "defaults"                  # → rw,suid,dev,exec,auto,nouser,async
+      "vers=3"                    # force NFSv3
+      "proto=tcp"                 # use TCP transport
+      "intr"                      # allow signals (Ctrl-C) to interrupt
+      "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
+      "retrans=3"                 # retry only 3 times (~9 s total)
+      "_netdev"                   # wait for network before mounting
+    ];
   };
 }
--- config/machines/servers/sower/hardware.nix
+++ config/machines/servers/sower/hardware.nix
@@ -45,8 +45,6 @@
         "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
         "retrans=3"                 # retry only 3 times (~9 s total)
         "_netdev"                   # wait for network before mounting
-        # "x-systemd.automount"       # lazy-mount on first access
-        # "x-systemd.idle-timeout=60" # auto-unmount after 60 s idle
       ];
     };
 
@@ -57,13 +55,10 @@
         "defaults"                  # → rw,suid,dev,exec,auto,nouser,async
         "vers=3"                    # force NFSv3
         "proto=tcp"                 # use TCP transport
-        "soft"                      # give up after retries (don’t hang forever)
         "intr"                      # allow signals (Ctrl-C) to interrupt
         "timeo=30"                  # initial timeout = 3 s (30 deciseconds)
         "retrans=3"                 # retry only 3 times (~9 s total)
         "_netdev"                   # wait for network before mounting
-        #"x-systemd.automount"       # lazy-mount on first access
-        #"x-systemd.idle-timeout=60" # auto-unmount after 60 s idle
       ];
     };
   };
