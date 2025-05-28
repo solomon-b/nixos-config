292005aa79fd53a5d3f1a479c21d5bc042d72516
solomon <ssbothwell@gmail.com>
2025-05-28 00:15:15 -0700

    sower: switch immich to nixpkgs module.

--- config/machines/servers/sower/immich.nix
+++ config/machines/servers/sower/immich.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 let
   dbHostname = "192.168.5.101"; # transfigured-night
@@ -10,28 +10,7 @@ let
   redisPassword = "hunter2";
   photosLocation = "/mnt/immich";
 
-  immichWebUrl = "http://immich_web:3000";
-  immichServerUrl = "http://immich_server:3001";
-  immichMachineLearningUrl = "http://immich_machine_learning:3003";
-
   typesenseApiKey = "abcxyz123";
-
-  environment = {
-    DB_HOSTNAME = dbHostname;
-    DB_USERNAME = dbUsername;
-    DB_PASSWORD = dbPassword;
-    DB_DATABASE_NAME = dbDatabaseName;
-
-    REDIS_HOSTNAME = redisHostname;
-    REDIS_PASSWORD = redisPassword;
-
-    UPLOAD_LOCATION = photosLocation;
-
-    IMMICH_WEB_URL = immichWebUrl;
-    IMMICH_SERVER_URL = immichServerUrl;
-    IMMICH_MACHINE_LEARNING_URL = immichMachineLearningUrl;
-
-  };
 in
 {
   fileSystems."/mnt/immich" = {
@@ -48,80 +27,53 @@ in
     ];
   };
 
-  virtualisation.oci-containers.containers = {
-    immich_server = {
-      imageFile = pkgs.dockerTools.pullImage {
-        imageName = "ghcr.io/immich-app/immich-server";
-        imageDigest = "sha256:e36c27ba1e3cd20bf6038d0b6b330e35a9a5b7ce9ee57db70a97bc49ca41ea6b";
-        sha256 = "sha256-phvsGAlPoh0RQQfkT9w3x+XYhTLp/U7JofETkJONrrM=";
-      };
-      image = "ghcr.io/immich-app/immich-server";
+  services.immich = {
+    enable = true;
+    machine-learning.enable = true;
 
-      ports = [ "8084:2283" ];
-
-
-      volumes = [
-        "${photosLocation}:/usr/src/app/upload"
-        "/etc/localtime:/etc/localtime:ro"
-      ];
-
-      environment = environment;
-
-      extraOptions = [ "--network=immich-bridge" ];
-
-      # config = {
-      #   WorkingDir = "/usr/src/app";
-      #   Entrypoint = [ "tini" "--" "/bin/bash" ];
-      #   Cmd = [ "start.sh" ];
-      # };
-
-      autoStart = true;
+    database = {
+      enable = false;
     };
 
-    immich_machine_learning = {
-      imageFile = pkgs.dockerTools.pullImage {
-        imageName = "ghcr.io/immich-app/immich-machine-learning";
-        imageDigest = "sha256:e00799ab188850df8d467723017610133c9e7e87fae454a3b015b8896093efa5";
-        sha256 = "sha256-zObsW6cR/5OT3XndOG0uoJDT3cJlGhpYavYbNZIRv/U=";
-      };
-      image = "ghcr.io/immich-app/immich-machine-learning";
-      extraOptions = [ "--network=immich-bridge" ];
-
-      environment = environment;
-
-      volumes = [
-        "${photosLocation}:/usr/src/app/upload"
-        "model-cache:/cache"
-      ];
-
-      autoStart = true;
+    redis = {
+      # Note: see above WRT to `database.enable`.
+      enable = false;
+      host = redisHostname;
     };
-  };
 
-  systemd.services.init-immich-network = {
-    description = "Create the network bridge for immich.";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      # Put a true at the end to prevent getting non-zero return code, which will
-      # crash the whole service.
-      check=$(${pkgs.docker}/bin/docker network ls | grep "immich-bridge" || true)
-      if [ -z "$check" ];
-        then ${pkgs.docker}/bin/docker network create immich-bridge
-        else echo "immich-bridge already exists in docker"
-      fi
-    '';
-  };
+    secretsFile = config.sops.secrets.immich-postgres-password.path;
 
+    environment = {
+      # PostgreSQL
+      DB_HOSTNAME = dbHostname;
+      DB_USERNAME = dbUsername;
+      DB_PASSWORD = dbPassword;
+      DB_DATABASE_NAME = dbDatabaseName;
+
+      # Redis
+      REDIS_HOSTNAME = redisHostname;
+      REDIS_PASSWORD = redisPassword;
+
+      # Upload path
+      UPLOAD_LOCATION = photosLocation;
+    };
+
+    mediaLocation = photosLocation;
+    settings.server.externalDomain = "http://immich.service.home.arpa";
+    settings.newVersionCheck.enabled = true;
+  };
 
   services.nginx.virtualHosts."immich.service.home.arpa" = {
     locations."/" = {
-      proxyPass = "http://localhost:8084";
+      proxyPass = "http://localhost:2283";
       extraConfig = ''
         client_max_body_size 0;
         proxy_max_temp_file_size 96384m;
       '';
     };
+  };
+
+  sops.secrets = {
+    immich-postgres-password = { };
   };
 }
