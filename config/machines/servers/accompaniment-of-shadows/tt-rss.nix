5dc61c987ee222ab4c2c2b993996b1de88fd4ff5
solomon <ssbothwell@gmail.com>
2025-06-04 02:02:36 -0700

    Moves tt_rss db password into sops.

--- config/machines/servers/accompaniment-of-shadows/tt-rss.nix
+++ config/machines/servers/accompaniment-of-shadows/tt-rss.nix
@@ -1,6 +1,6 @@
 # NOTE: Run the update script to initialize the database:
 # sudo -u tt_rss /nix/store/0ch7n28h724dpvkc30z18jgy21s9p49b-php-with-extensions-8.0.22/bin/php /var/lib/tt-rss/www/update.php --update-schema
-{ ... }:
+{ config, ... }:
 
 {
   services.tt-rss = {
@@ -9,11 +9,10 @@
     database = {
       host = "transfigured-night";
       port = 5432;
-      #TODO: Figure out why the passwordFile option isn't working or
-      #use agenix for secrets managment.
-      password = "password";
-      #passwordFile = "/secrets/tt_rss-postgres-password";
+      passwordFile = config.sops.secrets.tt_rss-password.path;
     };
     singleUserMode = true;
   };
+
+  sops.secrets.tt_rss-password = { };
 }
