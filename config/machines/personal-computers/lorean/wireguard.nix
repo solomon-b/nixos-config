64caeb16cfcc75d1763b35b4768a5d4778e5185c
solomon <ssbothwell@gmail.com>
2025-06-04 02:04:42 -0700

    lorean: remove deprecated wireguard interface.

--- config/machines/personal-computers/lorean/wireguard.nix
+++ config/machines/personal-computers/lorean/wireguard.nix
@@ -18,23 +18,4 @@
       ip46tables -t raw -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
     '';
   };
-
-  #networking.wireguard.interfaces = {
-  #  wg0 = {
-  #    ips = [ "10.100.0.4/24" ];
-  #    listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-  #    privateKeyFile = "/secrets/primary-user-wireguard-private-key";
-  #
-  #    peers = [
-  #      {
-  #        # TODO: Fetch password from pass
-  #        #publicKey = builtins.extraBuiltins.getFullPasswordValue pkgs "system/yellowstone/wireguard/public-key";
-  #        publicKey = "Y6OqeDXON8DZ83Hf4yGBekMWDtIPRzyvVxg0M9zqZxg=";
-  #        allowedIPs = [ "0.0.0.0/0" "::/0" ];
-  #        endpoint = "yellowstone.cofree.coffee:51820";
-  #        persistentKeepalive = 25;
-  #      }
-  #    ];
-  #  };
-  #};
 }
