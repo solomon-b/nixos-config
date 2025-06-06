8a15472b4b419a4bcb0c3cead8cf44d1870bb02b
solomon <ssbothwell@gmail.com>
2025-06-06 12:18:58 -0700

    Removes deprecated comment on sabnzbd

--- config/machines/servers/accompaniment-of-shadows/sabnzbd.nix
+++ config/machines/servers/accompaniment-of-shadows/sabnzbd.nix
@@ -3,13 +3,7 @@
 {
   services.sabnzbd = {
     enable = true;
-    #group = "downloads";
   };
-
-  ## TODO: Factor this out, since other downloads services depend on this group.
-  ##
-  ## Create the group for downloads.
-  #users.groups.downloads.gid = 1010;
 
   services.nginx.virtualHosts = {
     "sabnzbd.service.home.arpa" = {
