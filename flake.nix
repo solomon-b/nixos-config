5f3c30eec18b3f24d8c8410e30796dff0f0b7e92
solomon <ssbothwell@gmail.com>
2025-09-15 08:36:15 -0700

    chorse: Switch from unstable to 25.05.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-25.05;
     unstable.url = github:nixos/nixpkgs;
 
     flake-utils = {
@@ -8,7 +8,7 @@
     };
 
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = github:nix-community/home-manager/release-25.05;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
